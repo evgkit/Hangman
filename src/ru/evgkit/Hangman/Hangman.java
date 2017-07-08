e9a6fce49d6873a6d9befbed975ef02cf4929df1
evgkit <kitaev.ea@gmail.com>
2017-07-08 16:46:20 +0300

    Fix Hangman example

--- src/ru/evgkit/Hangman/Hangman.java
+++ src/ru/evgkit/Hangman/Hangman.java
@@ -4,8 +4,9 @@ public class Hangman {
 
     public static void main(String[] args) {
         if (0 == args.length) {
-            System.out.println("Please enter the word.");
-            System.exit(0);
+            System.out.println("Usage:  java Hangman <answer>");
+            System.err.println("answer is required");
+            System.exit(1);
         }
 
         Game game = new Game(args[0]);
